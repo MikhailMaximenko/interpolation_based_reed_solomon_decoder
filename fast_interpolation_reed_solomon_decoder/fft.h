15742e577a3ae417313f9008694f5bd405576abc
maximenko <maximenko@POLARIS>
2026-03-24 17:53:48 +0300

    add anonimous namespaces

--- fast_interpolation_reed_solomon_decoder/fft.h
+++ fast_interpolation_reed_solomon_decoder/fft.h
@@ -10,24 +10,24 @@ void FFT511(unsigned*, unsigned*);
 
 
 void inline call_fft(std::vector<unsigned>& src, std::vector<unsigned>& dst) {
-	if (src.size() <=64) {
+	if (src.size() <= 8) {
+		FFT7(src.data(), dst.data());
+		return;
+	}
+	if (src.size() <= 64) {
 		FFT63(src.data(), dst.data());
 		return;
 	}
-	//if (src.size() <= 63) {
-	//	FFT63(src.data(), dst.data());
-	//	return;
-	//}
-	//if (src.size() <= 127) {
-	//	FFT127(src.data(), dst.data());
-	//	return;
-	//}
-	//if (src.size() <= 255) {
-	//	FFT255(src.data(), dst.data());
-	//	return;
-	//}
-	//if (src.size() <= 511) {
-	//	FFT511(src.data(), dst.data());
-	//	return;
-	//}
+	if (src.size() <=128) {
+		FFT127(src.data(), dst.data());
+		return;
+	}
+	if (src.size() <= 256) {
+		FFT255(src.data(), dst.data());
+		return;
+	}
+	if (src.size() <= 512) {
+		FFT511(src.data(), dst.data());
+		return;
+	}
 }
